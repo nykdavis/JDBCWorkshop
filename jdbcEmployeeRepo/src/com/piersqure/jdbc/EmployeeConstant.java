24a34f0a55e39a7862f99dc74c6aea89245bf0ad
dvsnayak <yello.davis@gmail.com>
2024-07-30 07:45:05 +0530

    initial check up for jdbc code

---
+++ jdbcEmployeeRepo/src/com/piersqure/jdbc/EmployeeConstant.java
@@ -0,0 +1,25 @@
+package com.piersqure.jdbc;
+/**
+ * The EmployeeConstant class holds the database configuration constants 
+ * such as URL, Username, and password for the JDBC connection. These values 
+ * are used throughout the application to ensure a consistent configuration 
+ * and avoid hardcoding values in multiple places.
+ *
+ * <p>This is a utility class and should not be instantiated.</p>
+ * 
+ * <p>Developer: Davis</p>
+ * <p>Date: July 30, 2024</p>
+ */
+
+public class EmployeeConstant {
+	
+	private EmployeeConstant() {
+		
+	}
+	public static final String DB_URL = "jdbc:mysql://localhost:3306/GithubProjects" ;
+	public static final String USERNAME = "root" ;
+	public static final String PASSWORD = "root" ;
+	public static final String INSERT_QUERY = "INSERT INTO JDBCEmployee (id, name, department , salary) VALUES (?, ?, ?, ?)";
+	public static final String GET_QUERY = "SELECT * FROM JDBCEmployee"; 
+	public static final String DELETE_BY_ID_QUERY = "DELETE FROM JDBCEmployee WHERE id = ?";
+}
